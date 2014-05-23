739926de402bb8ec6d9abb812b8ea88b81e94f2f
Andrew Montague <apprentice@Maldives.local>
2014-05-23 16:51:42 +1200

    adding format to menu

--- views/home_view.rb
+++ views/home_view.rb
@@ -7,9 +7,13 @@ class HomeView
 
   # @return [String] user input
   def run(arguments)
+
     good_command_message = 'Please enter your command > '
     command_entry_message = arguments[:bad_command_message] || good_command_message
-    @writer.print_line 'Welcome to Ruby Challenger'
+    @writer.print_line "______________________________________
+------\e[4mWELCOME TO RUBY CHALLENGER\e[0m------
+
+\n-Press \e[4mq\e[0m to begin\n-Enter \e[4mquit\e[0m to exit\n "
     @writer.print_on_same_line command_entry_message
 
     input = gets.chomp
